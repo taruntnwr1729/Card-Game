7646978cc2e802a9e16b8db397d606035b124c7b
taruntnwr1729 <taruntanwar1729@gmail.com>
2026-05-27 02:11:24 +0530

    I added full UI of my app in this code

--- Card Game/ContentView.swift
+++ Card Game/ContentView.swift
@@ -9,15 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            //I added background here
+            Image("background-cloth")
+            //then I put logo and cards on top of it stacking vertically
+            VStack (){
+                Spacer(minLength: 125)
+                
+                Image("logo")
+                Spacer(minLength: 60)
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card12")
+                    Spacer()
+                }
+                Spacer(minLength: 40)
+                Image("button")
+                Spacer()
+                
+                
+                HStack{
+                    Spacer()
+                    VStack{
+                        
+                        Spacer()
+                        Text("Player 1")
+                            .font(.title)
+                        Text("0")
+                            .font(.title2)
+                            .padding(.bottom)
+                        Spacer(minLength: 30)
+                        
+                    }
+                    Spacer()
+                    VStack{
+                        Spacer()
+                        Text("Player 2")
+                            .font(.title)
+                        Text("0")
+                            .font(.title2)
+                            .padding(.bottom)
+                        Spacer()
+                        
+                    }
+                    Spacer()
+                    
+                    }
+                Spacer(minLength: 50)
+                }
+                
+                
+            }
+            .padding()
         }
-        .padding()
     }
-}
+
 
 #Preview {
     ContentView()
